4ad88f4d7ed3e400c9dc08c30e1b97966f67894d
Will <farahan7@msu.edu>
2025-03-17 10:24:15 -0400

    weapons!

--- Assets/_Scripts/Weapon.cs
+++ Assets/_Scripts/Weapon.cs
@@ -1,8 +1,10 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Random = UnityEngine.Random;
+
 
 public enum eWeaponType
 {
@@ -21,11 +23,13 @@ public class Weapon : MonoBehaviour
     public eWeaponType currentWeapon;
     private bool reFired = false; // This is used for accuracy calculations
     private Vector3 offsetVector;
+    
+    private bool fireCooldown = false;
 
     void Start()
     {
         
-        ChangeWeapon(eWeaponType.pistol);
+        ChangeWeapon(eWeaponType.shotgun);
     }
 
     void ChangeWeapon(eWeaponType weaponToSwitchTo)
@@ -39,36 +43,72 @@ public class Weapon : MonoBehaviour
             case eWeaponType.pistol:
                 weaponType = Resources.Load<WeaponSO>("WeaponSOs/pistol");  
                 break;
+            case eWeaponType.shotgun:
+                weaponType = Resources.Load<WeaponSO>("WeaponSOs/shotgun"); 
+                break;
         }
+        // Calculating how our fireCooldown. We have the shots per second thanks to decinos video
         
     }
     
-    void FixedUpdate()
+    void Update()
     {
+        // Firing
         if (Input.GetMouseButton(0))
         {
-            print(weaponType.weaponType);
-            if (reFired)
+            if(fireCooldown) return;
+            // Doom has a mechanic where the first shot will always be accurate
+            
+
+            for (int i = 0; i < weaponType.numberOfShots; i++)
             {
-                float offset = Random.Range(-weaponType.offsetMax, weaponType.offsetMax);
-                Quaternion forwardOffset = Quaternion.AngleAxis(offset, new Vector3(0, 1, 0));
-                offsetVector = forwardOffset * transform.forward;  
+                if (reFired || weaponType.weaponType == eWeaponType.shotgun)
+                {
+                    float offset = Random.Range(-weaponType.offsetMax, weaponType.offsetMax);
+                    Quaternion forwardOffset = Quaternion.AngleAxis(offset, new Vector3(0, 1, 0));
+                    offsetVector = forwardOffset * transform.forward;  
                 
+                }
+                else offsetVector = transform.forward;
+                Physics.Raycast(transform.position, offsetVector, out RaycastHit hit, weaponType.range);
+                
+                //For debugging
+                Vector3 forward = offsetVector * weaponType.range;
+                Debug.DrawRay(transform.position, forward, Color.red, 1);  
             }
-            else offsetVector = transform.forward;
-            Physics.Raycast(transform.position, offsetVector, out RaycastHit hit, weaponType.range);
-            print("firing");
-            //For debugging
-            Vector3 forward = offsetVector * weaponType.range;
-            Debug.DrawRay(transform.position, forward, Color.red, 1);
+            
             // Our first shot will always be accurate, however if we keep the mouse down our shots will be
             // Innacurate
             reFired = true;
+            if(!fireCooldown) fireCooldown = true;
+            Invoke("ShotRefresh", weaponType.rateOfFire);
+            
         }
-
         if (Input.GetMouseButtonUp(0))
         {
             reFired = false;
         }
+        
+        
+        // Switching weapons
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            ChangeWeapon(eWeaponType.none);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            ChangeWeapon(eWeaponType.pistol);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            ChangeWeapon(eWeaponType.shotgun);
+        }
+    }
+
+    void ShotRefresh()
+    {
+        fireCooldown = false;
     }
 }
--- Assets/_Scripts/WeaponSO.cs
+++ Assets/_Scripts/WeaponSO.cs
@@ -8,9 +8,12 @@ public class WeaponSO : ScriptableObject
     public eWeaponType weaponType;
     public float damageMin = 5f;
     public float damageMax = 15f;
+    [Tooltip("Shots per second")]
+    public float rateOfFire = 2.5f;
     public float range = 2048f;
     public GameObject ammoType = null;
     public int weaponSlot = 2;
     public float offsetMin = 0f;
     public float offsetMax = 5.6f;
+    public int numberOfShots = 1;
 }
